4a4a511b816b82aa81f28d798beee9c4aeb81ad4
steven <szapata427@yahoo.com>
2019-07-29 22:49:49 -0400

    deleted query search

--- flaskAPI/dbconnectionpractice.py
+++ flaskAPI/dbconnectionpractice.py
@@ -10,15 +10,6 @@ oneresumedatabase = mysql.connector.connect(
 )
 mycursor = oneresumedatabase.cursor()
 
-
-    # sqlFormula = "INSERT INTO accountinformation (FirstName, LastName, Email, Address1, Address2, City, State, Country) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
-
-    # secondMember = ("Nichole", "Zapata", "nicholezapata@gmail.com", "110 Romain Ave", "null", "Pompton Lakes", "NJ", "USA")
-
-    # mycursor.execute(sqlFormula, secondMember)
-    # oneresumedatabase.commit()
-
-
 mycursor.execute("select * from oneresumedatabase.accountinformation")
 result = mycursor.fetchall()
 print(result)
